efe922197cc6382ba355b152e85779ac42a755df
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-08-12 22:58:35 -0700

    customer.js framework done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-var connection = mysqlsql.createConnection({
+var connection = mysql.createConnection({
     host: 'localhost',
     port:3306,
     user:'root',
@@ -14,19 +14,75 @@ connection.connect(function(err){
         throw err;
     }
 
-    // console.log('response');
-    askCustomer();
+    
 });
 
+connection.query("SELECT * FROM products",function(err,res){
+    if(err){
+        throw err;
+    }
+    console.log(res);
+    
+
+// console.log('response');
+})  
+
+askCustomer();
+
 function askCustomer(){
-    inquirer.prompt({
+    inquirer.prompt([
+        {
         name: 'productId',
         type: 'number',
-        message:'Product ID, please.'
-    },
-    {
+        message:'Product ID, please.',
+        validate:function(value){
+            if(isNaN(value)===false && value >= 1 && value <=10){
+                return true;
+            }
+            return false;
+        }
+        },
+       {
         name:'count',
         type:'number',
-        message:'how many do you want?'
-    })
+        message:'how many do you want?',
+        validate:function(value){
+            if(isNaN(value) === false){
+                return true;
+            }
+            return false;
+        }
+        }
+    ])
+    .then(function(answer){
+        var buyId = answer.productId;
+        var buyCount = answer.count;
+        connection.query("SELECT * FROM products",function(err,res){
+            if(err){
+                throw err;
+            }
+            if(res[buyId-1].buyCount > res[buyId-1].stock_quantity){
+                console.log('Insufficient quantity!');
+            }else{
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [{
+                    stock_quantity: stock_quantity- buyCount
+                    },{item_id: buyId}],
+                    function(err){
+                        if(err){
+                            throw err;
+                        }
+
+                        var totalCost = res[buyId-1].price * buyCount; 
+                        console.log('Your total cost is' + totalCost);
+                    }
+        
+                }
+            // console.log(res);
+           // connection.end();
+        });
+    });
 };
+
+ 
