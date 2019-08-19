4a8bfb9fe193aa2d23ae909311247a5c441cf2a6
Xiaoquan Zhou <46883216+herbinmorn@users.noreply.github.com>
2019-08-19 15:52:15 -0700

    Update bamazonSupervisor.js

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port:3306,
     user:'root',
-    password:'517283730',
+    password:'Zxq@820121',
     database:'bamazon_db'
 });
 
